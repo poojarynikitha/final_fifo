c260bbffce34da60ca7dc80d2e90713b8ff6e6b6
poojarynikitha <126642704+poojarynikitha@users.noreply.github.com>
2023-09-19 20:07:35 +0530

    Update fifo_sequencer.sv

--- fifo_sequencer.sv
+++ fifo_sequencer.sv
@@ -1,4 +1,4 @@
-class fifo_sequencer extends uvm_sequencer#(fifo_sequence_item);
+class fifo_sequencer extends uvm_sequencer#(fifo_seq_item);
   `uvm_component_utils(fifo_sequencer)
   
   function new(string name = "fifo_sequencer", uvm_component parent);
