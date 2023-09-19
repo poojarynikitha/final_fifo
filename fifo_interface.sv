7acafecad67563eabf0f3f9ddd374b390d21d58e
poojarynikitha <126642704+poojarynikitha@users.noreply.github.com>
2023-09-19 20:12:51 +0530

    Update fifo_interface.sv

--- fifo_interface.sv
+++ fifo_interface.sv
@@ -1,36 +1,38 @@
 import uvm_pkg::*;
 `include "uvm_macros.svh"
-//`timescale 1ns/10ps
+`timescale 1ns/10ps
 
-interface fifo_interface(clk,rstn);
-  input clk,rstn;
-    bit o_full,o_empty,o_alm_full,o_alm_empty;
-    bit i_wren,i_rden;
-    bit [127:0] i_wrdata;
-    bit [127:0] o_rddata;
-
-
-clocking d_cb @(posedge clk);
+interface fifo_interface(input clk, rstn);
+  bit i_wren;
+  bit i_rden;
+  bit [127:0] i_wrdata;
+  bit o_full;
+  bit o_empty;
+  bit o_alm_full;
+  bit o_alm_empty;
+  bit [127:0] o_rddata;
+  
+  clocking dri_cb @(posedge clk);
     default input #1 output #1;
     output i_wren;
     output i_rden;
     output i_wrdata;
   endclocking
-
- clocking m_cb @(posedge clk);
+  
+  clocking mon_cb @(posedge clk);
     default input #1 output #1;
+    input o_full;
+    input o_empty;
+     input o_alm_full;
+  input o_alm_empty;
+    input o_rddata;
     input i_wren;
     input i_rden;
     input i_wrdata;
-    input o_full;
-    input o_empty;
-    input o_alm_full;
-   input o_alm_empty;
-   input o_rddata;
   endclocking
 
-modport d_mp (input clk, rstn, clocking d_cb);
-  modport m_mp (input clk, rstn, clocking m_cb);
+  modport dri_mp (input clk, rstn, clocking dri_cb);
+  
+      modport mon_mp (input clk, rstn, clocking mon_cb);
     
 endinterface
-  
