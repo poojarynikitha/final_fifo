7e54db8a68aba236f8b83cef725621ce6676119c
poojarynikitha <126642704+poojarynikitha@users.noreply.github.com>
2023-09-19 19:55:17 +0530

    Update fifo_driver.sv

--- fifo_driver.sv
+++ fifo_driver.sv
@@ -1,55 +1,45 @@
-
-
-class fifo_driver extends uvm_driver#(fifo_seq_item);
-  virtual fifo_if vif;
-  fifo_seq_item tr;
+class fifo_driver extends uvm_driver#(fifo_sequence_item);
+  virtual fifo_interface vif;
+  fifo_sequence_item req;
   `uvm_component_utils(fifo_driver)
   
   function new(string name = "fifo_driver", uvm_component parent);
     super.new(name, parent);
   endfunction
-  
-  virtual function void build_phase(uvm_phase phase);
+
+   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    if(!uvm_config_db#(virtual fifo_if)::get(this, "", "vif", vif))begin
+     if(!uvm_config_db#(virtual fifo_interface)::get(this, "", "vif", vif))
       `uvm_fatal("Driver: ", "No vif is found!")
-    end
   endfunction
 
-  virtual task run_phase(uvm_phase phase);
+        virtual task run_phase(uvm_phase phase);
+    vif.d_mp.d_cb.i_wren <= 'b0;                // signals are registered to DUT
+    vif.d_mp.d_cb.i_rden <= 'b0;
+    vif.d_mp.d_cb.i_wrdata <= 'b0;
     forever begin
-      if(vif.d_mp.d_cb.rstn==0) begin
-        @(posedge vif.d_mp.d_cb)
-        vif.d_mp.d_cb.i_wren <= 'b0;
-        vif.d_mp.d_cb.i_rden <= 'b0;
-        vif.d_mp.d_cb.i_wrdata <= 'b0;
-      end
-        seq_item_port.get_next_item(tr);
-        if(tr.i_wren == 1)begin
-          main_write(tr.i_wrdata);
-        end
-//         else
-          if(tr.i_rden == 1)begin
-          main_read();
-        end
-        seq_item_port.item_done();
-      end
+      seq_item_port.get_next_item(req);        
+      if(req.i_wren == 1)                      // req represents a transaction that specifies whether a write or read operation is to be performed on the FIFO.
+        main_write(req.i_wrdata);
+      else if(req.i_rden == 1)
+        main_read();
+      seq_item_port.item_done();
+    end
   endtask
-  
-  virtual task main_write(input [`DATA_W - 1:0] din);
-      @(posedge vif.d_mp.d_cb)
-      vif.d_mp.d_cb.i_wren <= 'b1;
-      vif.d_mp.d_cb.i_wrdata <= din;
-//     $display("time = %0t, driver -> writedata = %0h",$time,din);
-//     $display("time = %0t, driver -> vif.d_mp.d_cb.i_wrdata = %0h",$time,vif.d_mp.d_cb.i_wrdata);
-      @(posedge vif.d_mp.d_cb)
-      vif.d_mp.d_cb.i_wren <= 'b0;
+
+     virtual task main_write(input [127:0] din);
+    @(posedge vif.d_mp.clk)
+    vif.d_mp.d_cb.i_wren <= 'b1;
+    vif.d_mp.d_cb.i_wrdata <= din;
+    @(posedge vif.d_mp.clk)
+    vif.d_mp.d_cb.i_wren <= 'b0;
   endtask
-  
+
+      
   virtual task main_read();
-    @(posedge vif.d_mp.d_cb)
+    @(posedge vif.d_mp.clk)
     vif.d_mp.d_cb.i_rden <= 'b1;
-    @(posedge vif.d_mp.d_cb)
+    @(posedge vif.d_mp.clk)
     vif.d_mp.d_cb.i_rden <= 'b0;
   endtask
 
